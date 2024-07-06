71dfb997274372a70937bf49cee68a89bfd7428a
Meister-anon <user@DESKTOP-TQ65PS9>
2024-07-06 15:10:28 -0400

    updates credits file

--- include/resource+credits_file.h
+++ include/resource+credits_file.h
@@ -1,4 +1,5 @@
 #define CREDITS
+/*
 //point of this file, to store references to easily get back to them
 //when I need to. think will put credits on firered game start pika page
 //simple easy unobtrusive
@@ -6,3 +7,12 @@
 //for HGSS dex stuff credit EGG & Jaizu  maybe xaman too?
 
 //Pokabbie's native battle speed up
+
+
+//Show Type Effectiveness In Battle w Option Menu Toggle
+// by devolov  & PokemonCrazy
+-Couldn't direclty translate to FireRed so took inspiration from
+my version is coded pretty much entirely differently
+
+Plan add Ghoul Slash bag sort  -  ghoul bag-sort
+*/
