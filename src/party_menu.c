bb70bc0862b66480de699ea75ecd99b0fc8b95d5
Meister-anon <user@DESKTOP-TQ65PS9>
2025-04-21 19:48:18 -0400

    party menu change clarification

--- src/party_menu.c
+++ src/party_menu.c
@@ -4077,9 +4077,10 @@ static void Task_SlideSelectedSlotsOnscreen(u8 taskId)
         PutWindowTilemap(sPartyMenuBoxes[gPartyMenu.slotId].windowId);
         PutWindowTilemap(sPartyMenuBoxes[gPartyMenu.slotId2].windowId);
         ScheduleBgCopyTilemapToVram(0);
-        // BUG: memory leak - is this the fix or the cause?
-         Free(sSlot1TilemapBuffer);
-         Free(sSlot2TilemapBuffer);
+        // BUG: memory leak - is this the fix or the cause? //checked EE thisis the fix...
+        //but setting this breaks my game...
+        Free(sSlot1TilemapBuffer);
+        Free(sSlot2TilemapBuffer);
         FinishTwoMonAction(taskId);
     }
     // Continue sliding
