6bbe778e44baa0f8eeb4ed72c88d9f7848d5c47e
Meister-anon <user@DESKTOP-TQ65PS9>
2025-04-30 18:29:29 -0400

    extra missed comma in base stats

--- src/data/pokemon/base_stats.h
+++ src/data/pokemon/base_stats.h
@@ -48198,7 +48198,7 @@ const struct BaseStats gBaseStats[] =
         .floating = TRUE,
         .levelUpLearnset = sMarowakLevelUpLearnset,
         .tmhmLearnset = sMarowakTMHMLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, RELATIVE_EVO(38, MEDIUM_EFFORT), 0,  SPECIES_BEINFAE}),, //Should hopefully blank these for now without issue
+        .evolutions = EVOLUTION({EVO_LEVEL, RELATIVE_EVO(38, MEDIUM_EFFORT), 0,  SPECIES_BEINFAE}), //Should hopefully blank these for now without issue
     },//good but relatively avearge not very strong physically
     //idea is somewhat like larvesta evolves into something like psuedolegend/mythical
     //think all black eyes a satchel made from persons tooth actually a tooth since mon is small
