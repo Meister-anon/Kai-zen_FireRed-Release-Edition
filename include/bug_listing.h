c240f194a9039c11f472144d237f83f1d27e3621
Meister-anon <user@DESKTOP-TQ65PS9>
2025-11-16 15:58:38 -0500

    update bug list

--- include/bug_listing.h
+++ include/bug_listing.h
@@ -6,4 +6,13 @@ for player only foe?
 modern noticed anticipation not procing
 
 
+-11/5/25 - unresolved
+dmging moves that lower stats get reflected
+to setting battler, believe is a result of somehow
+activating mirror armor refelction but unsure.
+
+also of note Anercomp seemed to have activated 
+this effect (constrict on caterpie) and it caused a game freeze
+-potential fix finish battle controller rework and battle script refactor
+see if that fixes issue.
 */
