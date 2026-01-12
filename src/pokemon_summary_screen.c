f21ae83e7099696b0a7ee86e4c5b4fd6602904c3
Meister-anon <user@DESKTOP-TQ65PS9>
2026-01-12 16:06:33 -0500

    replace use of gMoveDescriptionPointers

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -3884,7 +3884,7 @@ static void PokeSum_PrintSelectedMoveStats(void)
                                      7, 12,
                                      0, 0,
                                      sLevelNickTextColors[0], TEXT_SKIP_DRAW,
-                                     gMoveDescriptionPointers[sMonSummaryScreen->moveIds[sMoveSelectionCursorPos]]);
+                                     GetMoveDescription(sMonSummaryScreen->moveIds[sMoveSelectionCursorPos]));
     }
 }
 
