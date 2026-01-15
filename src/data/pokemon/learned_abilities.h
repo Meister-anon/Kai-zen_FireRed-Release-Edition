cc8480a65ff757eca126546d4863177ea24ff080
Meister-anon <user@DESKTOP-TQ65PS9>
2026-01-15 10:09:54 -0500

    maractus buffs

--- src/data/pokemon/learned_abilities.h
+++ src/data/pokemon/learned_abilities.h
@@ -2523,6 +2523,8 @@ static const struct AbilityLearnset sDarmanitanAbilityLearnset[] = {
 static const struct AbilityLearnset sMaractusAbilityLearnset[] = {
     ABILITY_DESERT_DWELLER,
     ABILITY_TOUGH_SPINES,
+    ABILITY_CHLOROPHYLL,
+    ABILITY_GRASS_PELT,
     ABILITY_SET_END
 };
 
