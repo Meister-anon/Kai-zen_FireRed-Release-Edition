8514b5252a7a2515ae217dc05696cdc9316e9ad7
Meister-anon <user@DESKTOP-TQ65PS9>
2026-01-25 15:41:20 -0500

    remove old loadcompresspal define

--- include/palette.h
+++ include/palette.h
@@ -63,7 +63,6 @@ extern u16 ALIGNED(4) gPlttBufferUnfaded[PLTT_BUFFER_SIZE];
 extern u16 ALIGNED(4) gPlttBufferFaded[PLTT_BUFFER_SIZE];
 
 
-void LoadPalette(const u32 *src, u16 offset, u16 size);
 void LoadPalette(const void *src, u16 offset, u16 size);
 void FillPalette(u16 value, u16 offset, u16 size);
 void TransferPlttBuffer(void);
