a9e9d44519bdf1551d0094eef730b5c5fee9db34
Meister-anon <user@DESKTOP-TQ65PS9>
2026-01-28 01:35:08 -0500

    more update for func renames

--- src/trade.c
+++ src/trade.c
@@ -2123,6 +2123,7 @@ static void SignalRedrawTradeMenus(u8 monIdx)
     }
 }
 
+//is DrawSelectedMonScreen in EE
 static void HandleRedrawTradeMenuOnSide(u8 side)
 {
     s8 nameStringWidth;
@@ -2158,7 +2159,7 @@ static void HandleRedrawTradeMenuOnSide(u8 side)
         gSprites[sTradeMenuResourcesPtr->partyIcons[0][partyIdx + (whichParty * PARTY_SIZE)]].data[4] = (sTradeMonSpriteCoords[whichParty * PARTY_SIZE][1] * 8) - 12;
         StoreSpriteCallbackInData6(&gSprites[sTradeMenuResourcesPtr->partyIcons[0][partyIdx + (whichParty * PARTY_SIZE)]], SpriteCB_MonIcon);
         sTradeMenuResourcesPtr->menuRedrawState[side]++;
-        StartSpriteLinearTranslationFromCurrentPos(&gSprites[sTradeMenuResourcesPtr->partyIcons[0][partyIdx + (whichParty * PARTY_SIZE)]]);
+        Trade_MoveSelectedMonToTarget(&gSprites[sTradeMenuResourcesPtr->partyIcons[0][partyIdx + (whichParty * PARTY_SIZE)]]);
         CopyToBgTilemapBufferRect_ChangePalette(1, sTradePartyBoxTilemap, side * 15, 0, 15, 17, 0);
         CopyBgTilemapBufferToVram(1);
         CopyBgTilemapBufferToVram(0);
