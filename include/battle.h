0e2acb2e4b989d5c8e30faf636fb9ead60980d93
Meister-anon <user@DESKTOP-TQ65PS9>
2026-02-06 03:46:33 -0500

    turn off unused struct value

--- include/battle.h
+++ include/battle.h
@@ -1132,7 +1132,7 @@ struct BattleStruct
     u8 printedStrongWindsWeakenedAttack:1;
     u8 numSpreadTargets:3;
     u8 moldBreakerActive:1;
-    struct MessageStatus slideMessageStatus;
+    //struct MessageStatus slideMessageStatus; vsonic
     u8 trainerSlideSpriteIds[MAX_BATTLERS_COUNT];
     //unsure if need above 2
     u8 hazardsQueue[NUM_BATTLE_SIDES][HAZARDS_MAX_COUNT];
