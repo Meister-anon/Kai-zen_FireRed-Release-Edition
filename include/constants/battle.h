dafd3971b2bf36b53ad9d23a4b836b49c959cc79
Meister-anon <user@DESKTOP-TQ65PS9>
2026-02-13 04:51:50 -0500

    add back gmax move effects plus ai fix

--- include/constants/battle.h
+++ include/constants/battle.h
@@ -1316,9 +1316,65 @@ enum __attribute__((packed)) MoveEffect
     MOVE_EFFECT_EERIE_SPELL,
     //zmove and lets go move effects
     MOVE_EFFECT_AROMATHERAPY,
+
+    //not using added for compile
+    // Max move effects happen earlier in the execution chain.
+    // For example stealth rock from G-Max Stonesurge is set up before abilities but from Stone Axe after.
+    // Stone Axe can also fail to set up rocks if user faints where as Stonesurge will always go up.
+    // This means we need to be careful if we want to re-use those effects for (new) vanilla moves
+    MOVE_EFFECT_RAISE_TEAM_ATTACK,
+    MOVE_EFFECT_RAISE_TEAM_DEFENSE,
+    MOVE_EFFECT_RAISE_TEAM_SPEED,
+    MOVE_EFFECT_RAISE_TEAM_SP_ATK,
+    MOVE_EFFECT_RAISE_TEAM_SP_DEF,
+    MOVE_EFFECT_LOWER_ATTACK_SIDE,
+    MOVE_EFFECT_LOWER_DEFENSE_SIDE,
+    MOVE_EFFECT_LOWER_SPEED_SIDE,
+    MOVE_EFFECT_LOWER_SP_ATK_SIDE,
+    MOVE_EFFECT_LOWER_SP_DEF_SIDE,
+    MOVE_EFFECT_SUN,
+    MOVE_EFFECT_RAIN,
+    MOVE_EFFECT_SANDSTORM,
+    MOVE_EFFECT_HAIL,
+    MOVE_EFFECT_MISTY_TERRAIN,
+    MOVE_EFFECT_GRASSY_TERRAIN,
+    MOVE_EFFECT_ELECTRIC_TERRAIN,
+    MOVE_EFFECT_PSYCHIC_TERRAIN,
+    MOVE_EFFECT_VINE_LASH,
+    MOVE_EFFECT_WILDFIRE,
+    MOVE_EFFECT_CANNONADE,
+    MOVE_EFFECT_EFFECT_SPORE_SIDE,
+    MOVE_EFFECT_PARALYZE_SIDE,
+    MOVE_EFFECT_CONFUSE_PAY_DAY_SIDE,
+    MOVE_EFFECT_CRIT_PLUS_SIDE,
+    MOVE_EFFECT_PREVENT_ESCAPE_SIDE,
+    MOVE_EFFECT_AURORA_VEIL,
+    MOVE_EFFECT_INFATUATE_SIDE,
+    MOVE_EFFECT_RECYCLE_BERRIES,
+    MOVE_EFFECT_POISON_SIDE,
+    MOVE_EFFECT_DEFOG,
+    MOVE_EFFECT_POISON_PARALYZE_SIDE,
+    MOVE_EFFECT_HEAL_TEAM,
+    MOVE_EFFECT_SPITE,
+    MOVE_EFFECT_GRAVITY,
+    MOVE_EFFECT_VOLCALITH,
+    MOVE_EFFECT_SANDBLAST_SIDE,
+    MOVE_EFFECT_YAWN_FOE,
+    MOVE_EFFECT_LOWER_EVASIVENESS_SIDE,
+    MOVE_EFFECT_AROMATHERAPY,
+    MOVE_EFFECT_CONFUSE_SIDE,
+    MOVE_EFFECT_STEELSURGE, // Steel type rocks
+    MOVE_EFFECT_STEALTH_ROCK, // Max Move rocks, not to be confused for rocks set up from Ceasless Edge (same but differ in execution order)
+    MOVE_EFFECT_TORMENT_SIDE,
+    MOVE_EFFECT_LOWER_SPEED_2_SIDE,
+    MOVE_EFFECT_FIRE_SPIN_SIDE,
+    MOVE_EFFECT_FIXED_POWER,
+    // Max move effects end. They can be used for (custom) normal moves.
+
     // Move effects that happen before the move hits. Set in SetPreAttackMoveEffect
     MOVE_EFFECT_BREAK_SCREEN,
     MOVE_EFFECT_STEAL_STATS,
+    MOVE_EFFECT_BEAT_UP_MESSAGE, // Handles the message printing for gen2,3 and 4
     //custom effects
     MOVE_EFFECT_INFESTATION, //bug status
     MOVE_EFFECT_SWITCH_LOCKED, //need this to set status and timer can remove effect
--- src/battle_ai_main.c
+++ src/battle_ai_main.c
@@ -6144,9 +6144,6 @@ static s32 AI_CheckViability(enum BattlerId battlerAtk, enum BattlerId battlerDe
 // Effects that are encouraged on the first turn of battle
 static s32 AI_ForceSetupFirstTurn(enum BattlerId battlerAtk, enum BattlerId battlerDef, u32 move, s32 score)
 {
-    u32 abilityAtk = AI_DATA->abilities[battlerAtk];
-    u32 abilityDef = AI_DATA->abilities[battlerDef];
-
     if (IsTargetingPartner(battlerAtk, battlerDef)
       || gBattleResults.battleTurnCounter != 0)
         return score;
