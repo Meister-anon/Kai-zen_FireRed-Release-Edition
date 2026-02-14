EIGHTFINITE/pokeemerald-expansion

Trainerbattle Type Differentiation (#6424)
Co-authored-by: CallmeEchoo <callmeechoo@github.com>
Co-authored-by: Your Name <you@example.com>

src/battle_message.c
@@ -4,7 +4,7 @@
 #include "battle_controllers.h"
 #include "battle_message.h"
 #include "battle_setup.h"
-#include "battle_tower.h"
+#include "battle_special.h"
 #include "battle_z_move.h"
 #include "data.h"
 #include "event_data.h"

src/pokemon.c
@@ -12,6 +12,7 @@
 #include "battle_tower.h"
 #include "battle_z_move.h"
 #include "data.h"
+#include "frontier_util.h"
 #include "daycare.h"
 #include "dexnav.h"
 #include "event_data.h"

Meister-anon/Kai-zen_FireRed-Release-Edition

src/battle_message.c
@@ -4,10 +4,12 @@
 #include "battle.h"
 #include "battle_anim.h"
 #include "battle_util.h"
+#include "battle_z_move.h"
 #include "strings.h"
 #include "battle_message.h"
 #include "link.h"
 #include "event_scripts.h"
+#include "data.h"
 #include "event_data.h"
 #include "item.h"
 #include "battle_tower.h"

src/pokemon.c
@@ -8,6 +8,13 @@
 #include "battle_anim.h"
 #include "item.h"
 #include "item_use.h"
+#include "battle_controllers.h"
+#include "battle_message.h"
+#include "battle_setup.h"
+#include "battle_tower.h"
+#include "battle_z_move.h"
+#include "data.h"
+#include "daycare.h"
 #include "event_data.h"
 #include "util.h"
 #include "money.h"