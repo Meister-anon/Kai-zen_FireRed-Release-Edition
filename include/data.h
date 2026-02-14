c39963df0f5856731f133a3c354b96d33ed185ca
Meister-anon <user@DESKTOP-TQ65PS9>
2026-02-14 03:12:06 -0500

    fix bad include

--- include/data.h
+++ include/data.h
@@ -2,7 +2,7 @@
 #define GUARD_DATA_H
 
 #include "global.h"
-#include "opponents.h"
+#include "constants/opponents.h"
 
 #define SPECIES_SHINY_TAG 500
 #define TRAINER_ENCOUNTER_MUSIC(trainer)((gTrainers[trainer].encounterMusic_gender & 0x7F))
