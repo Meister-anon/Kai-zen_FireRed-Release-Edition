809d9831bbdcf038d23cfe356cb2d20cd144f9ea
Meister-anon <user@DESKTOP-TQ65PS9>
2026-02-16 13:18:07 -0500

    update item define

--- include/constants/items.h
+++ include/constants/items.h
@@ -1184,6 +1184,19 @@ enum __attribute__((packed)) Item
 //#define NUM_TECHNICAL_MACHINES 161 //change to 120 later  / or more plan to add on to tms I think for ease spreading certain moves
 //#define NUM_HIDDEN_MACHINES     9
 
+//amount hp berries heal
+//#if B_CONFUSE_BERRIES_HEAL >= GEN_8
+    #define CONFUSE_BERRY_HEAL_FRACTION 3
+/*#elif B_CONFUSE_BERRIES_HEAL == GEN_7
+    #define CONFUSE_BERRY_HEAL_FRACTION 2
+#else
+    #define CONFUSE_BERRY_HEAL_FRACTION 8
+#endif*/
+
+//at what hp threshold they trigger
+#define CONFUSE_BERRY_HP_FRACTION /*((B_CONFUSE_BERRIES_HEAL >= GEN_7) ?*/ 4 //: 2)
+
+
 //copy from emerald for replacing the numerical values in type field in items.json
 // Item type IDs (used to determine the exit callback)
 //#define ITEM_USE_MAIL           0
