cb7672699ee3a5a48036cd6eaca9bb35da3e3bed
Meister-anon <user@DESKTOP-TQ65PS9>
2026-02-28 01:39:03 -0500

    major define fixes

--- include/battle_setup.h
+++ include/battle_setup.h
@@ -106,6 +106,7 @@ const u8 *BattleSetup_GetTrainerPostBattleScript(void);
 void ShowTrainerCantBattleSpeech(void);
 void PlayTrainerEncounterMusic(void);
 const u8 *GetTrainerALoseText(void);
+const u8 *GetTrainerBLoseText(void);
 const u8 *GetTrainerWonSpeech(void);
 
 //stuff from EE //wont need rematch stuff
