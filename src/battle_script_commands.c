3b8930e5e3314022fc136dfadb3def03632ad279
Meister-anon <user@DESKTOP-TQ65PS9>
2026-03-02 17:37:25 -0500

    fixed missed value

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -7487,7 +7487,7 @@ static void PutMonIconOnLvlUpBanner(void)
     u32 species = GetMonData(mon, MON_DATA_SPECIES);
     u32 personality = GetMonData(mon, MON_DATA_PERSONALITY);
 
-    iconSheet.data = GetMonIconPtr(species, personality);
+    iconSheet.data = GetMonIconPtr(species, personality, 1); //idk what frame is for here, do emerald icons not move? 
     iconSheet.size = 0x200;
     iconSheet.tag = TAG_LVLUP_BANNER_MON_ICON;
 
